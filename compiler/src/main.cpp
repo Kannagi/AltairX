25f6edce314bffc178e618a5185014cc5f1ea915
Alairion <alexy.pellegrini@laposte.net>
2021-02-17 23:49:51 +0100

    basic files

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -1,4 +1,24 @@
-int main()
-{
+#include <iostream>
 
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/SourceMgr.h>
+
+int main(int argc, char **argv)
+{
+    if(argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <IR file>\n";
+        return 1;
+    }
+
+    llvm::LLVMContext context;
+    llvm::SMDiagnostic err;
+    std::unique_ptr<llvm::Module> mod = llvm::parseIRFile(argv[1], err, context);
+
+    if(!mod)
+    {
+        std::cerr << "RIP\n";
+        return 1;
+    }
 }
