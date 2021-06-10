cb63b858dcf00d99ef35baa551c071f0d1314b81
Kannagi <kannagichan@gmail.com>
2021-06-10 23:57:29 +0200

    MMap

--- vasm/half.c
+++ vasm/half.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-	float fval = 0.00006103515625;
-	unsigned int *tmp = (void*)&fval;
-	unsigned int val = *tmp;
-	unsigned int sign,exp,mant,texp;
+	float fval = 1.5;
+	unsigned int val;
+	memcpy(&val, &fval, sizeof(val));
 
+	unsigned int sign,exp,mant,texp;
 
 	//float to half
 	sign = (val>>16)&0x8000; //sign
@@ -18,15 +18,14 @@ int main()
 	if(texp&0x80)
 		exp |= 0x4000;
 
-	//printf("%x\n",sign);
-	//printf("%x\n",exp);
-	//printf("%x\n",mant);
-
 	unsigned int half = sign+mant+exp;
 	printf("half %x\n",half);
-	unsigned int xval;
+
+
+
 
 	//half to float
+	unsigned int xval;
 	exp = (half&0x3C00)>>3;
 
 	if(half&0x4000)
@@ -38,24 +37,16 @@ int main()
 		if(exp != 0)
 			exp |= 0x3800;
 	}
-	
+
 	exp = exp<<16;
-
-
 
 	xval  = (half&0x8000)<<16;
 	xval += (half&0x03FF)<<13;
 	xval += exp;
 
+	memcpy(&fval, &xval, sizeof(xval));
 
-	//printf("%x\n",sign);
-	//printf("%x\n",exp);
-	//printf("%x\n",mant);
-
-	//xval = sign+mant+exp;
-	float *ftmp = (void*)&xval;
-
-	printf("%f %x/%x\n",*ftmp,xval,val);
+	printf("%f %x/%x\n",fval,xval,val);
 
 	return 0;
 }
